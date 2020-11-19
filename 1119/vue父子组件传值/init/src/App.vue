ab786a91ded8c90227d2cf09a62d3e90137d1f89
hcyhuchaoyue <hcyhuchaoyue@163.com>
2020-11-19 21:30:56 +0800

    vue父子组件传值

---
+++ 1119/vue父子组件传值/init/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h2>我是父组件的data：我现在在父组件里面{{ data }}</h2>
+    <HelloWorld
+      :data="data"
+      @changeData="changeHelloWorld"
+      @childzdata="parentFn"
+    />
+    <h2>我是父组件接收到的子组件的zdata：我现在在父组件里面{{ zdata }}</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      data: 0,
+      zdata: "",
+    };
+  },
+  methods: {
+    // 子组件修改父组件的值
+    changeHelloWorld(params) {
+      this.data += params;
+    },
+    // 子组件给父组件传值
+    parentFn(payload) {
+      this.zdata = payload;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ 1119/vue父子组件传值/init/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h2>我是子组件接收到的父组件的data：我现在在子组件里面{{ data }}</h2>
+    <button @click="changeFu">修改父组件的值</button>
+    <h2>我是子组件的zdata：我现在在子组件里面{{ zdata }}</h2>
+    <button @click="ziTofu">子组件给父组件传值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    data: {
+      type: Number,
+      default: 1,
+    },
+  },
+  data() {
+    return {
+      zdata: 10,
+    };
+  },
+  methods: {
+    changeFu() {
+      this.$emit("changeData", 2);
+    },
+    ziTofu() {
+      console.log("childzdata");
+      this.$emit("childzdata", this.zdata);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
